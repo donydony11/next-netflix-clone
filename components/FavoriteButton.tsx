77010c5563de9698af75550de8a911cd3acb0cee
donydony11 <121674533+donydony11@users.noreply.github.com>
2023-04-12 17:35:29 +0300

    first commit

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,31 +1,31 @@
-import axios from 'axios';
-import React, { useCallback, useMemo } from 'react';
+import axios from 'axios'
+import React, { useCallback, useMemo } from 'react'
 import { AiOutlinePlus, AiOutlineCheck } from 'react-icons/ai'
-import useCurrentUser from '@/hooks/useCurrentUser';
-import useFavorites from '@/hooks/useFavorites';
+import useCurrentUser from '@/hooks/useCurrentUser'
+import useFavorites from '@/hooks/useFavorites'
 
 interface FavoriteButtonProps {
   movieId: string
 }
 
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
-  const { mutate: mutateFavorites } = useFavorites();
+  const { mutate: mutateFavorites } = useFavorites()
 
-  const { data: currentUser, mutate } = useCurrentUser();
+  const { data: currentUser, mutate } = useCurrentUser()
 
   const isFavorite = useMemo(() => {
     const list = currentUser?.favoriteIds || [];
 
-    return list.includes(movieId);
-  }, [currentUser, movieId]);
+    return list.includes(movieId)
+  }, [currentUser, movieId])
 
 const toggleFavorites = useCallback(async () => {
   let response;
 
   if (isFavorite) {
-    response = await axios.delete(`/api/favorite?movieId=${movieId}`);
+    response = await axios.delete(`/api/favorite?movieId=${movieId}`)
   } else {
-    response = await axios.post('/api/favorite', { movieId });
+    response = await axios.post('/api/favorite', { movieId })
   }
 
   const updatedFavoriteIds = response?.data?.favoriteIds;
@@ -33,9 +33,9 @@ const toggleFavorites = useCallback(async () => {
   mutate({ 
     ...currentUser, 
     favoriteIds: updatedFavoriteIds,
-  });
+  })
   mutateFavorites();
-}, [movieId, isFavorite, currentUser, mutate, mutateFavorites]);
+}, [movieId, isFavorite, currentUser, mutate, mutateFavorites])
   
   const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus;
 
